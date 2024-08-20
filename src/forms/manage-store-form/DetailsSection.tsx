024dcb66036caa04ab4b19af684baaa4263c1a8f
Omolemo Kgosietsile Diale <omodiale.codes@gmail.com>
2024-08-20 13:40:10 +0200

    stores ui update

--- src/forms/manage-store-form/DetailsSection.tsx
+++ src/forms/manage-store-form/DetailsSection.tsx
@@ -7,21 +7,21 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useFormContext } from "react-hook-form";
 
 const DetailsSection = () => {
   const { control } = useFormContext();
-
   return (
     <div className="space-y-2">
       <div>
         <h2 className="text-2xl font-bold">Details</h2>
-        <FormDescription>Enter the details about your store</FormDescription>
+        <FormDescription>
+          Enter the details about your store
+        </FormDescription>
       </div>
       <FormField
         control={control}
-        name="storeName"
+        name="restaurantName"
         render={({ field }) => (
           <FormItem>
             <FormLabel>Name</FormLabel>
@@ -82,30 +82,6 @@ const DetailsSection = () => {
             <FormLabel>Estimated Delivery Time (minutes)</FormLabel>
             <FormControl>
               <Input {...field} className="bg-white" placeholder="30" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-
-      {/* Status Field */}
-      <FormField
-        control={control}
-        name="status"
-        render={({ field }) => (
-          <FormItem className="max-w-[25%]">
-            <FormLabel>Status</FormLabel>
-            <FormControl>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <SelectTrigger className={`bg-${field.value === 'online' ? 'green-500' : field.value === 'busy' ? 'yellow-500' : 'red-500'}`}>
-                  <SelectValue placeholder="Select status" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="online">Online</SelectItem>
-                  <SelectItem value="busy">Busy</SelectItem>
-                  <SelectItem value="offline">Offline</SelectItem>
-                </SelectContent>
-              </Select>
             </FormControl>
             <FormMessage />
           </FormItem>
